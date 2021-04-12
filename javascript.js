8c6babb59365fdc1a4a8fd33604f62fcfc0035e4
Jack Ritchie <jwf.ritchie@gmail.com>
2021-04-12 20:24:53 +1000

    two working api calls

--- javascript.js
+++ javascript.js
@@ -1,12 +1,20 @@
 $(document).ready(function(){
     $.ajax({
-        url: "https://geoip-db.com/jsonp",
-        jsonpCallback: "callback",
-        dataType: "jsonp",
-        success: function(location) {
-            console.log(location);
-        }
-
-    })
-
+		url: "https://geolocation-db.com/jsonp",
+		jsonpCallback: "callback",
+		dataType: "jsonp",
+		success: function( location ) {
+			$('#country').html(location.country_name);
+			$('#state').html(location.state);
+			$('#city').html(location.city);
+			$('#latitude').html(location.latitude);
+			$('#longitude').html(location.longitude);
+			$('#ip').html(location.IPv4); 
+		 
+		}
+		
+	});	
+	$.get( "https://covid-api.mmediagroup.fr/v1/cases", function(data) {
+		console.log(data.Global.All.confirmed);
+	  });
 });
