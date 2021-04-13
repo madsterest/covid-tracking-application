2cfa72fbb67ac3f3fbffc263efaeaeb89ab25438
madsterest <odea.madeleine@gmail.com>
2021-04-13 17:37:07 +1000

    Added fetch calls for the data. Created event listeners and a init function

--- javascript.js
+++ javascript.js
@@ -1,18 +1,47 @@
 // Get the input from the form using event listener. "submit" for the form and "click" for the button.
 var button = document.querySelector("button");
 var formInput = document.getElementById("search-focus");
+var form = document.querySelector("form");
 
 button.addEventListener("click", function () {
   console.log("hey there");
   var countryName = formInput.value.trim();
   console.log(countryName);
-  var requestUrl =
-    "https://covid-api.mmediagroup.fr/v1/vaccines?country=" + countryName;
-  vaccineAPI(requestUrl);
 });
 
-function vaccineAPI(URL) {
-  fetch(URL)
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log("submitted");
+  var countryName = formInput.value.trim();
+  console.log(countryName);
+});
+
+// function vaccineAPI(locationName) {
+//   var requestUrl =
+//     "https://covid-api.mmediagroup.fr/v1/vaccines?country=" + locationName;
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//     });
+// }
+
+function covidStats(locationName) {
+  var requestUrl =
+    "https://covid-api.mmediagroup.fr/v1/cases?country=" + locationName;
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+
+  var requestVaccineUrl =
+    "https://covid-api.mmediagroup.fr/v1/vaccines?country=" + locationName;
+  fetch(requestVaccineUrl)
     .then(function (response) {
       return response.json();
     })
@@ -21,6 +50,23 @@ function vaccineAPI(URL) {
     });
 }
 
+function getLocation() {
+  url = "https://geolocation-db.com/json/f9902210-97f0-11eb-a459-b997d30983f1";
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var countryName = data.country_name;
+      console.log(data);
+      console.log(countryName);
+      covidStats(countryName);
+    });
+}
+
+function init() {
+  getLocation();
+}
 // $(document).ready(function () {
 //   $.ajax({
 //     url: "https://geolocation-db.com/jsonp",
@@ -40,3 +86,4 @@ function vaccineAPI(URL) {
 //     console.log(data);
 //   });
 // });
+init();
