2e65f118a8f3a8a65b08160ae3a309ee58e6f568
madsterest <odea.madeleine@gmail.com>
2021-04-13 19:01:43 +1000

    Reviewed Java Code

--- javascript.js
+++ javascript.js
@@ -2,11 +2,14 @@
 var button = document.querySelector("button");
 var formInput = document.getElementById("search-focus");
 var form = document.querySelector("form");
+var locationDisplay = document.querySelector(".card-title");
+var statsDisplay = document.querySelector(".card-text");
 
 button.addEventListener("click", function () {
   console.log("hey there");
   var countryName = formInput.value.trim();
   console.log(countryName);
+  locationDisplay.innerHTML = "Location: " + countryName;
 });
 
 form.addEventListener("submit", function (event) {
@@ -14,6 +17,7 @@ form.addEventListener("submit", function (event) {
   console.log("submitted");
   var countryName = formInput.value.trim();
   console.log(countryName);
+  locationDisplay.innerHTML = "Location: " + countryName;
 });
 
 // function vaccineAPI(locationName) {
@@ -61,29 +65,12 @@ function getLocation() {
       console.log(data);
       console.log(countryName);
       covidStats(countryName);
+      locationDisplay.innerHTML = "Current Location: " + countryName;
     });
 }
 
 function init() {
   getLocation();
 }
-// $(document).ready(function () {
-//   $.ajax({
-//     url: "https://geolocation-db.com/jsonp",
-//     jsonpCallback: "callback",
-//     dataType: "jsonp",
-//     success: function (location) {
-//       $("#country").html(location.country_name);
-//       $("#state").html(location.state);
-//       $("#city").html(location.city);
-//       $("#latitude").html(location.latitude);
-//       $("#longitude").html(location.longitude);
-//       $("#ip").html(location.IPv4);
-//     },
-//   });
-//   $.get("https://covid-api.mmediagroup.fr/v1/cases", function (data) {
-//     console.log(data.Global.All.confirmed);
-//     console.log(data);
-//   });
-// });
+
 init();
