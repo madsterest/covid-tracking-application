af9fa92e7f4d4f7e653e6cc0f0c6cb314b0aa091
Stephanie Lee <stephaniemjlee@gmail.com>
2021-04-14 23:57:15 +1000

    updated js file - main stats content. updated html

--- javascript.js
+++ javascript.js
@@ -3,57 +3,103 @@ var button = document.querySelector("button");
 var formInput = document.getElementById("search-focus");
 var form = document.querySelector("form");
 var locationDisplay = document.querySelector(".card-title");
-var statsDisplay = document.querySelector(".card-text");
+var casesDisplay = document.querySelector(".card-subtitle");
+var vaccineStats = document.querySelector("#vaccinatedStats");
+var deathStats = document.querySelector("#deathStats");
+var percentageStats = document.querySelector("#percentageStats");
+var safeRanking = document.querySelector("#safeRank");
 
-button.addEventListener("click", function () {
-  console.log("hey there");
-  var countryName = formInput.value.trim();
-  console.log(countryName);
-  locationDisplay.innerHTML = "Location: " + countryName;
-});
+// button.addEventListener("click", function () {
+//   var countryName = formInput.value.trim();
+//   console.log(countryName);
+//   covidStats(countryName);
+// });
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  console.log("submitted");
-  var countryName = formInput.value.trim();
-  console.log(countryName);
-  locationDisplay.innerHTML = "Location: " + countryName;
-});
+// form.addEventListener("submit", function (event) {
+//   event.preventDefault();
+//   console.log("submitted");
+//   var countryName = formInput.value.trim();
+//   console.log(countryName);
+// });
 
-// function vaccineAPI(locationName) {
-//   var requestUrl =
-//     "https://covid-api.mmediagroup.fr/v1/vaccines?country=" + locationName;
-//   fetch(requestUrl)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//     });
-// }
+//used below as results only showed when the button was manually clicked and not when pressing enter on the keyboard.  
+function onLocationSubmit(event){
+  event.preventDefault()
+      var countryName = formInput.value.trim();
+      console.log(countryName);
+      covidStats(countryName);
+}
+
+function formatLocationName(locationName) {
+  // title case location name
+  var titleCasedLocation = locationName.replace(/\w\S*/g, function(txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+  // uri encode location name
+  console.log(encodeURI(titleCasedLocation));
+  return encodeURI(titleCasedLocation);
+}
 
 function covidStats(locationName) {
+  locationDisplay.innerHTML = locationName;
+
+  // format locationName to title case and uri encode
+  var formattedLocationName = formatLocationName(locationName);
+
   var requestUrl =
-    "https://covid-api.mmediagroup.fr/v1/cases?country=" + locationName;
+    "https://covid-api.mmediagroup.fr/v1/cases?country=" + formattedLocationName;
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
+      var currentCases =
+        data.All.confirmed - data.All.deaths - data.All.recovered;
+      console.log(currentCases);
+      casesDisplay.innerHTML = "Active cases: " + currentCases;
+
+      var percentage = ((currentCases / data.All.population) * 100).toFixed(4);  
+      console.log(percentage);
+      percentageStats.innerHTML = "Percentage of active cases in total population: " + percentage + "%";
+
+      if(percentage <=10) {
+        safeRanking.innerHTML = "Safe to travel";
+        safeRanking.style.color = "green";
+      } else if(percentage >10 && percentage <=30) {
+        safeRanking.innerHTML = "Take care";
+        safeRanking.style.color = "yellow";
+      } else {
+        safeRanking.innerHTML = "DO NOT TRAVEL";
+        safeRanking.style.color = "red";
+      };
+
+      var currentDeaths = 
+        data.All.deaths;
+      console.log(currentDeaths)
+      deathStats.innerHTML = "Deaths: " + currentDeaths;
+
     });
 
   var requestVaccineUrl =
-    "https://covid-api.mmediagroup.fr/v1/vaccines?country=" + locationName;
+    "https://covid-api.mmediagroup.fr/v1/vaccines?country=" + formattedLocationName;
+    console.log('formattedLocationName', formattedLocationName);
   fetch(requestVaccineUrl)
     .then(function (response) {
+      console.log({response, formattedLocationName});
       return response.json();
     })
     .then(function (data) {
       console.log(data);
+      var vaccinated = data.All.people_partially_vaccinated;
+      console.log(vaccinated)
+      vaccineStats.innerHTML = "Vaccinated: " + vaccinated;
     });
-}
 
+    }
+
+
+// Initial page. Current location and stats displayed
 function getLocation() {
   url = "https://geolocation-db.com/json/f9902210-97f0-11eb-a459-b997d30983f1";
   fetch(url)
@@ -64,13 +110,13 @@ function getLocation() {
       var countryName = data.country_name;
       console.log(data);
       console.log(countryName);
-      covidStats(countryName);
-      locationDisplay.innerHTML = "Current Location: " + countryName;
+      covidStats(countryName)
     });
 }
 
-function init() {
-  getLocation();
-}
 
-init();
+// function init() {
+  getLocation();
+// }
+
+// init();
